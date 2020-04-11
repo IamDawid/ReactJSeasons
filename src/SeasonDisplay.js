1e7e274c7219cca852195be95fe711fa7b831f15
Dawid <david.d.bialy@gmail.com>
2020-04-11 20:43:46 +0100

    - season-themed CSS styling added

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
 const seasonConfig = {
@@ -31,9 +32,11 @@ const SeasonDisplay = (props) => {
 
 
     return <div>
-                    <i className={`${iconName} icon`} />
+                 <div className={`season-display ${season}`}>
+                    <i className={`icon-left massive ${iconName} icon`} />
                     <h1>{text}</h1>
-                    <i className={`${iconName} icon`} />
+                    <i className={`icon-right massive ${iconName} icon`} />
+                 </div>
            </div>;
 };
 
