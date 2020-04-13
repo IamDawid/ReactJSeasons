8ca669c4b58f959b28f421a3f3c20ce1e1ad4fcd
Dawid <david.d.bialy@gmail.com>
2020-04-13 21:36:37 +0100

    - Spinner now shows appropriate message. Also, default message defined.

--- src/index.js
+++ src/index.js
@@ -34,8 +34,8 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat} />
         }
 
-        
-        return <Spinner />
+
+        return <Spinner message="Please accept location request" />
         
 
         }
